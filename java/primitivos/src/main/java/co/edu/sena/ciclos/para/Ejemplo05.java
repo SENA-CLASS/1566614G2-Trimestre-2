244dcd04a88e73dd7ccf4aa9523c4ce6dd4a97d8
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2018-05-02 11:21:51 -0500

    chore: ejemplos UML Diagrama de clases y formatos de casos de uso extendido

--- java/primitivos/src/main/java/co/edu/sena/ciclos/para/Ejemplo05.java
+++ java/primitivos/src/main/java/co/edu/sena/ciclos/para/Ejemplo05.java
@@ -3,7 +3,12 @@ package co.edu.sena.ciclos.para;
 public class Ejemplo05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int  arreglo[] = {1,2,3,4,5,6,7,8,9};
+		
+		for (int i : arreglo) {
+			
+		}
+		
 
 	}
 
